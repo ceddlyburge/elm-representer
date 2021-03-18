8e2f6e88cd26396e7304b9a147037f6b7a0e26da
cedd burge <ceddburge@cedds-MBP.fritz.box>
2021-03-18 15:20:52 +0000

    Add test for record pattern

--- tests/Pattern.elm
+++ tests/Pattern.elm
@@ -13,5 +13,10 @@ suite =
                 |> whenNormalize                
                 |> thenContains "IDENTIFIER_1 (IDENTIFIER_2, IDENTIFIER_3) =\nIDENTIFIER_2 + IDENTIFIER_3"
 
+    , test "shoud normalize record patterns" <|
+            \_ ->
+                givenElmCodeOf "add {x, y} =\n    x + y"
+                |> whenNormalize                
+                |> thenContains "IDENTIFIER_1 {IDENTIFIER_2, IDENTIFIER_3} =\nIDENTIFIER_2 + IDENTIFIER_3"
                 
     ]
