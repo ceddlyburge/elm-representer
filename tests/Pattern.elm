606ef4ea7982a13bc2c9f18655421eb4fa5c3318
cedd burge <ceddburge@cedds-MBP.fritz.box>
2021-03-18 15:25:00 +0000

    Add test for uncons pattern

--- tests/Pattern.elm
+++ tests/Pattern.elm
@@ -7,16 +7,21 @@ import Helpers exposing (..)
 suite : Test
 suite =
     describe "Normalize"
-    [ test "shoud normalize tuple patterns" <|
+    [ test "shoud normalize tuple pattern" <|
             \_ ->
                 givenElmCodeOf "add (x, y) =\n    x + y"
                 |> whenNormalize                
                 |> thenContains "IDENTIFIER_1 (IDENTIFIER_2, IDENTIFIER_3) =\nIDENTIFIER_2 + IDENTIFIER_3"
 
-    , test "shoud normalize record patterns" <|
+    , test "shoud normalize record pattern" <|
             \_ ->
                 givenElmCodeOf "add {x, y} =\n    x + y"
                 |> whenNormalize                
                 |> thenContains "IDENTIFIER_1 {IDENTIFIER_2, IDENTIFIER_3} =\nIDENTIFIER_2 + IDENTIFIER_3"
                 
+    , test "shoud normalize uncons pattern" <|
+            \_ ->
+                givenElmCodeOf "add x :: xs =\n    x :: xs"
+                |> whenNormalize                
+                |> thenContains "IDENTIFIER_1 IDENTIFIER_2 :: IDENTIFIER_3 =\nIDENTIFIER_2 :: IDENTIFIER_3"
     ]
