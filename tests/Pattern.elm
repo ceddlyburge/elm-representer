d2b0244c5c98079bb595cdc3b476f621c5837f99
cedd burge <ceddburge@cedds-MBP.fritz.box>
2021-03-18 15:40:02 +0000

    Add test for named pattern

--- tests/Pattern.elm
+++ tests/Pattern.elm
@@ -25,11 +25,37 @@ suite =
                 |> whenNormalize                
                 |> thenContains "IDENTIFIER_1 IDENTIFIER_2 :: IDENTIFIER_3 =\nIDENTIFIER_2 :: IDENTIFIER_3"
 
-                
     , test "shoud normalize list pattern" <|
             \_ ->
                 givenElmCodeOf "add [x1, x2] =\n    x1 + x2"
                 |> whenNormalize                
                 |> thenContains "IDENTIFIER_1 [IDENTIFIER_2, IDENTIFIER_3] =\nIDENTIFIER_2 + IDENTIFIER_3"
+
+
+
+    , test "shoud normalize names pattern" <|
+            \_ ->
+                givenElmCodeOf "value (Node a) =\n    a"
+                |> whenNormalize                
+                |> thenContains "IDENTIFIER_1 ( Node IDENTIFIER_2 ) =\nIDENTIFIER_2"
+
+    -- use for case expression when done
+    -- , test "shoud normalize named pattern" <|
+    --         \_ ->
+    --             givenElmCodeOf """default1 maybeX =
+    -- case maybeX of
+    --     Just x ->
+    --         x
+        
+    --     Nothing ->
+    --         1"""
+    --             |> whenNormalize                
+    --             |> thenContains """IDENTIFIER_1 IDENTIFIER_2 =
+    -- case IDENTIFIER_2
+    --     Just IDENTIFIER_3 ->
+    --         IDENTIFIER_3
+        
+    --     Nothing ->
+    --         1"""
                 
     ]
