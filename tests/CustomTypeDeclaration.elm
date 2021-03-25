5707a8437117717694a5005539fe24416e9f45bc
Cedd Burge <ceddlyburge@gmail.com>
2021-03-25 21:31:06 +0000

    Update elm-syntax and restore previously failing tests

--- tests/CustomTypeDeclaration.elm
+++ tests/CustomTypeDeclaration.elm
@@ -1,35 +1,25 @@
-module CustomTypeDeclaration exposing (..)
-
---exposing (suite)
+module CustomTypeDeclaration exposing (suite)
 
 import Helpers exposing (..)
 import Test exposing (..)
 
 
-temp =
-    1
-
-
-
--- suite : Test
--- suite =
---     describe "Normalize"
---         [ skip <|
---             test "shoud normalize Name and ValueConstructors of Custom Types" <|
---                 \_ ->
---                     givenElmCodeOf "type FirstName = FirstName String"
---                         |> whenNormalize
---                         |> thenContains "type Identifier_1\n=Identifier_1 String"
---         , skip <|
---             test "shoud normalize generic type parameters of Custom Types" <|
---                 \_ ->
---                     givenElmCodeOf "type InputType a = InputType a String"
---                         |> whenNormalize
---                         |> thenContains "type Identifier_1 identifier_2\n=Identifier_1 identifier_2 String"
---         , skip <|
---             test "shoud ignore typeclass type parameters Custom Types" <|
---                 \_ ->
---                     givenElmCodeOf "type InputType a = InputType a number"
---                         |> whenNormalize
---                         |> thenContains "type Identifier_1 identifier_2\n=Identifier_1 identifier_2 number"
---         ]
+suite : Test
+suite =
+    describe "Normalize"
+        [ test "shoud normalize Name and ValueConstructors of Custom Types" <|
+            \_ ->
+                givenElmCodeOf "type FirstName = FirstName String"
+                    |> whenNormalize
+                    |> thenContains "type Identifier_1\n=Identifier_1 String"
+        , test "shoud normalize generic type parameters of Custom Types" <|
+            \_ ->
+                givenElmCodeOf "type InputType a = InputType a String"
+                    |> whenNormalize
+                    |> thenContains "type Identifier_1 identifier_2\n=Identifier_1 identifier_2 String"
+        , test "shoud ignore typeclass type parameters Custom Types" <|
+            \_ ->
+                givenElmCodeOf "type InputType a = InputType a number"
+                    |> whenNormalize
+                    |> thenContains "type Identifier_1 identifier_2\n=Identifier_1 identifier_2 number"
+        ]
